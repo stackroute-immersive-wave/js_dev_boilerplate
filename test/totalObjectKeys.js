f1acc1afe59f167ec954ffa621d9271e1f8d6699
Dhoni7 <sangee481@wipro.com>
2017-01-30 09:21:25 +0000

    objkey

--- test/totalObjectKeys.js
+++ test/totalObjectKeys.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 module.exports = (function () {
     let totalNoObjects = 0, totalNoKeys = 0;
   return {
@@ -8,15 +7,6 @@ module.exports = (function () {
   function traverse(obj) {
         if (obj instanceof Array) {
         totalNoObjects = totalNoKeys + 1;
-=======
-module.exports = (function() {
-    let totalNoObjects = 0;
-    let totalNoKeys = 0;
-  // Traverse expected json
-  function traverse(obj) {
-        if (obj instanceof Array) {
-        totalNoObjects = totalNoObjects + 1;
->>>>>>> b6b85062a3ed1cede9f07cf3cda441d5db805779
             obj.forEach(function (value) {
             if (typeof value === 'object' && value) {
                 traverse(value);
@@ -25,11 +15,7 @@ module.exports = (function() {
             }
           });
       } else {
-<<<<<<< HEAD
           totalNoObjects = totalNoKeys + 1;
-=======
-            totalNoObjects = totalNoObjects + 1;
->>>>>>> b6b85062a3ed1cede9f07cf3cda441d5db805779
           for (let prop in obj) {
               if (typeof obj[prop] === 'object' && obj[prop]) {
                   traverse(obj[prop]);
@@ -43,10 +29,4 @@ module.exports = (function() {
         totalNoKeys: totalNoKeys
       };
   }
-<<<<<<< HEAD
-=======
-  return {
-    traverse: traverse
-  };
->>>>>>> b6b85062a3ed1cede9f07cf3cda441d5db805779
 })();
