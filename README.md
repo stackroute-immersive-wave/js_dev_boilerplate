9b84a822a27599e8b9df73dfcef887b000a65a85
Ximanta <ximanta.sarma@gmail.com>
2017-04-13 19:31:47 +0530

    Cleaned unrequired files

--- README.md
+++ README.md
@@ -21,10 +21,24 @@ Install gulp globally
 
 	$ sudo npm install gulp -g
 
-To build the app
+To test the app
 
 	$ npm run build
 
 To run the app
 
 	$ npm run execute
+
+### Package structure
+
+js_dev_boilerplate
+	/css
+	/html_docs
+	/inputdata --Input CSV files are here
+	/js --Cadet puts their data munging assignments here with  their name appended to file name
+	/outputdata --Students add the generated JSON here with  their name appended to file name
+	/test --I added few tests here and students added their own
+		/dataMungingSpec.js  --Tests to check proper API use
+		/testJsonStructureSpec.js (Uses totalObjectKeys.js and jsondiff.js of Hobbes)
+		/*.js Added by student
+		/ExpectedJSON -- JSON files expected to be generated
--- test/testJsonStructureSpec.js
+++ test/testJsonStructureSpec.js
@@ -1,33 +1,24 @@
 var chai = require('chai');
 var expect = chai.expect;
-
 var jsonDiff = require('./jsondiff');
 var totalObjectKeys = require('./totalObjectKeys');
-
-var expectedJSON = require('./expectedjson/lifeExpectancy.json');
-var actualJSON = require('../outputdata/Male_Female_Expentency_Nitin.json');
-
-var expectedJSON = require('./expectedjson/expectedJsonNavin1.json');
-var actualJSON = require('../outputdata/OutputJSONNavin1.json');
-
+var expectedJSON = require('./expectedjson/expectedJsonKeerthi.json');
+var actualJSON = require('../outputdata/outputjsonKeerthi.json');
 describe('Test Application as Blackbox', function(){
-
-  it('Test JSON is well formed', function(done){
-
+  it ('Test JSON is well formed', function(done){
     done();
   });
-
   it('JSON has expected Number of Objects', function(done){
-    var objMatrix = totalObjectKeys.traverse(actualJSON);
-    expect(objMatrix.totalNoObjects).to.not.equal(0);
-    expect(objMatrix.totalNoKeys).to.not.equal(0);
+       var expectedObjMatrix = totalObjectKeys.traverse(expectedJSON);
+       var actualObjMatrix = totalObjectKeys.traverse(actualJSON);
+    //To DO
+    //expect(actualObjMatrix.totalNoObjects).to.equal(expectedObjMatrix.totalNoObjects);
+    //expect(actualObjMatrix.totalNoKeys).to.equal(expectedObjMatrix.totalNoKeys);
     done();
   });
-
   it('Test JSON is as expected', function(done){
     var compareResult = jsonDiff.compareJSONObjects(expectedJSON, actualJSON);
     expect(compareResult.diffs).equal(0);
     done();
   });
-
 })
