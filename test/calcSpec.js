1e3dcb4f10bf4c14bccb02e2424b85bde3e45a86
Ximanta <ximanta.sarma@gmail.com>
2017-05-08 15:33:31 +0530

    Sample CI demo pass test

--- test/calcSpec.js
+++ test/calcSpec.js
@@ -6,6 +6,6 @@ add = require("../js/calc");
 describe.only("Test add method of calc", function(){
 		it("should return the addition of two numbers", function() {
            var result = add(5,10); 
-  	       result.should.be.equal(14);             
+  	       result.should.be.equal(15);             
     });
   });
