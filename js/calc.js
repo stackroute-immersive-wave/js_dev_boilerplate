f3f514182d6393991719f218c2177ce8b58fa6c8
umamaheswari <2012umak@gmail.com>
2017-05-09 16:44:52 +0530

    clear errors and commit

--- js/calc.js
+++
@@ -1,9 +0,0 @@
-
-/* eslint-disable */
-module.exports = function add(num1, num2) {
-	if(typeof num1 !== 'number' || typeof num2 !== 'number') {
-		throw Error('Incorrect parameters');
-	}
-	return num1 + num2;
-}
-
