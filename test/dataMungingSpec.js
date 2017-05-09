be8acc731777078344fbfd5ea8a198f2a3fd6a7f
umamaheswari <2012umak@gmail.com>
2017-05-09 13:52:46 +0530

    commit dataMunging

--- test/dataMungingSpec.js
+++ test/dataMungingSpec.js
@@ -3,7 +3,7 @@ expect = require("chai").expect,
 sinon = require('sinon'),
 readline = require("readline"),
 fs=require("fs"),
-convert = require("../js/chicagoCrimeAyushee.js");
+convert = require("../js/g20Uma");
 
 
 describe("A series of test for Converting  CSV to JSON",
@@ -39,4 +39,33 @@ describe("A series of test for Converting  CSV to JSON",
         expect(convert.bind(undefined, Number(1960))).to.not.throw(Error, "Not a number");
         done();
     });
+
+   describe("Test createInterface method of readline", function(err){
+        it("should be called only once", function() {
+           var spyCreateInterface = sinon.spy(readline, 'createInterface');
+           convert(2016);
+           readline.createInterface.restore();
+           sinon.assert.calledOnce(spyCreateInterface);
+   });
+ });
+   describe("Test on method of Interface for line event", function(err){
+   it("should be called", function() {
+          var stub = sinon.stub(readline.Interface.prototype, 'on');
+          convert(2016);
+          sinon.assert.called(stub);
+          readline.Interface.prototype.on.restore();
+          sinon.assert.calledWith(stub,"line");
+
+  });
+  });
+
+  describe("Test on method of Interface for close event", function(err){
+   it("should be called", function() {
+          var stub = sinon.stub(readline.Interface.prototype,'on');
+          convert(2016);
+          readline.Interface.prototype.on.restore();
+          sinon.assert.calledWith(stub,"close");
+   });
+
+   });
 });
