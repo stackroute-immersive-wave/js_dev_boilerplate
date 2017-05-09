7213fce43ff882b9f89f168759845c17f1e485c8
Mitali <mitaligarg94@gmail.com>
2017-05-09 15:14:03 +0530

    modified some cases

--- test/calcSpec.js
+++ test/calcSpec.js
@@ -1,18 +1,16 @@
-/*eslint-disable*/
 const should = require("chai").should(),
 expect = require("chai").expect,
 sinon = require('sinon'),
 add = require("../js/calc");
+
 describe.only("Test add method of calc", function(){
 		it("should return the addition of two numbers", function() {
-           var result = add(5,10); 
-  	       result.should.be.equal(15);             
-    });		
-		
-it('should fail if two parameters are not present', function(done){
-        expect(add.bind(undefined, 10)).to.throw(Error, "Incorrect parameters");
-        done();
+           var result = add(5,10);
+  	       result.should.be.equal(15);
     });
-
+<<<<<<< HEAD
+  });
+=======
 
   });
+>>>>>>> a4acdeda2d77b624c9b878e4fb2cc3942e73ddc5
--- test/dataMungingSpec.js
+++ test/dataMungingSpec.js
@@ -1,17 +1,17 @@
+
 const should = require("chai").should(),
 expect = require("chai").expect,
 sinon = require('sinon'),
 readline = require("readline"),
 fs=require("fs"),
 
-convert = require("../js/g20Mitali");
-
+convert = require("../js/sample");
 
 describe("A series of test for Converting  CSV to JSON",
 	function(err){
 
   it("should return sucess message", function(done){
-  	var result = convert(2013);
+  	var result = convert(2001);
   	result.should.be.equal('JSON written successfully');
     done();
     });
@@ -35,4 +35,39 @@ describe("A series of test for Converting  CSV to JSON",
         expect(convert.bind(undefined, '1960')).to.not.throw(Error, "Not a number");
         done();
     });
+
+   it('should not fail if the year is a Number object', function(done){
+        expect(convert.bind(undefined, Number(1960))).to.not.throw(Error, "Not a number");
+        done();
+    });
+});
+
+
+   describe("Test createInterface method of readline", function(err){
+        it("should be called only once", function() {
+           var spyCreateInterface = sinon.spy(readline, 'createInterface');
+           convert(2016);
+           readline.createInterface.restore();
+           sinon.assert.calledOnce(spyCreateInterface);
+   });
+ });
+   describe("Test on method of Interface for line event", function(err){
+   it("should be called", function() {
+          var stub = sinon.stub(readline.Interface.prototype, 'on');
+          convert(2016);
+          sinon.assert.called(stub);
+          readline.Interface.prototype.on.restore();
+          sinon.assert.calledWith(stub,"line");
+
+   });
+  });
+
+   describe("Test on method of Interface for close event", function(err){
+   it("should be called", function() {
+          var stub = sinon.stub(readline.Interface.prototype,'on');
+          convert(2016);
+          readline.Interface.prototype.on.restore();
+          sinon.assert.calledWith(stub,"close");
+   });
+
  });
