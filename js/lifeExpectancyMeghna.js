ee690199c2108496ebc693bf4f0b96efdebad5df
Meghna <meghnanvt70@gmail.com>
2017-05-09 13:11:40 +0530

    test cases cleared

--- js/lifeExpectancyMeghna.js
+++ js/lifeExpectancyMeghna.js
@@ -1,5 +1,17 @@
+/*eslint-disable*/
 module.exports = function convert(startYear)
 {
+	
+	if(typeof startYear=='string')
+	{
+   return "";
+ 	}
+
+if(typeof startYear !== 'number' || isNaN(startYear))
+ {
+       throw new Error('Not a number');
+ }
+
 	const readline = require('readline');
 const fs = require('fs');
 
@@ -13,16 +25,16 @@ const rl = readline.createInterface({
 	//  console.log(cleanedLine);
 
 
-	  if(cleanedLine[1]=="KHM" ||cleanedLine[1]=="AGO" ||cleanedLine[1]=="DEU" ||cleanedLine[1]=="MYS" ||cleanedLine[1]=="BTN" )
+	  if(cleanedLine[1]==="KHM" ||cleanedLine[1]==="AGO" ||cleanedLine[1]==="DEU" ||cleanedLine[1]==="MYS" ||cleanedLine[1]==="BTN" )
 	  	{
-			if(cleanedLine[3]=="SP.DYN.LE00.FE.IN")
+			if(cleanedLine[3]==="SP.DYN.LE00.FE.IN")
 			{
 				obj={};
 				obj["CountryName"]=cleanedLine[0];
 				obj["Female"]=cleanedLine[5];
 				obj["Year"]=cleanedLine[4];
 			}
-			if(cleanedLine[3]=="SP.DYN.LE00.MA.IN")	
+			if(cleanedLine[3]==="SP.DYN.LE00.MA.IN")	
 			{			
 				obj["Male"]=cleanedLine[5];
 				arr.push(obj);
