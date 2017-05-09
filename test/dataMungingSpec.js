950d74ed1ea27907dab464df395e0e984114fd0b
admin <kavya.dhurai31@gmail.com>
2017-05-09 14:29:13 +0530

    All test cases passed

--- test/dataMungingSpec.js
+++ test/dataMungingSpec.js
@@ -3,47 +3,47 @@ expect = require("chai").expect,
 sinon = require('sinon'),
 readline = require("readline"),
 fs=require("fs"),
-convert = require("../js/calc");
-//convert = require("../js/");
 
-// describe("A series of test for Converting  CSV to JSON",
-// 	function(err){
+convert = require("../js/lifeExpectancyKavya.js");
 
-//   it("should return sucess message", function(done){
-//   	var result = convert(2001);
-//   	result.should.be.equal('JSON written successfully');
-//     done();
-//     });
+describe("A series of test for Converting  CSV to JSON",
+    function(err){
 
-//    it('should fail if year is notprovided', function(done){
-//         expect(convert).to.throw(Error, "Not a number");
-//         done();
-//     });
+ it("should return sucess message", function(done){
+      var result = convert(2001);
+      result.should.be.equal('JSON written successfully');
+    done();
+    });
 
-//   it('should fail if year is not a number', function(done){
-//         expect(convert.bind(undefined, {})).to.throw(Error, "Not a number");
-//         done();
-//     });
+  it('should fail if year is notprovided', function(done){
+        expect(convert).to.throw(Error, "Not a number");
+        done();
+    });
 
-//    it('should fail if year is NaN', function(done){
-//         expect(convert.bind(undefined, NaN)).to.throw(Error, "Not a number");
-//         done();
-//     });
+ it('should fail if year is not a number', function(done){
+        expect(convert.bind(undefined, {})).to.throw(Error, "Not a number");
+        done();
+    });
 
-//    it('should not fail if the year is a literal number', function(done){
-//         expect(convert.bind(undefined, '1960')).to.not.throw(Error, "Not a number");
-//         done();
-//     });
+  it('should fail if year is NaN', function(done){
+        expect(convert.bind(undefined, NaN)).to.throw(Error, "Not a number");
+        done();
+    });
 
-//    it('should not fail if the year is a Number object', function(done){
-//         expect(convert.bind(undefined, Number(1960))).to.not.throw(Error, "Not a number");
-//         done();
-//     });
-// });
+  it('should not fail if the year is a literal number', function(done){
+        expect(convert.bind(undefined, '1960')).to.not.throw(Error, "Not a number");
+        done();
+    });
+
+  it('should not fail if the year is a Number object', function(done){
+        expect(convert.bind(undefined, Number(1960))).to.not.throw(Error, "Not a number");
+        done();
+    });
+});
 
 
 describe("Test createInterface method of readline", function(err){
-		it("should be called only once", function() {
+        it("should be called only once", function() {
             var spyCreateInterface = sinon.spy(readline, 'createInterface');
             convert(2016);
             readline.createInterface.restore();
@@ -58,10 +58,10 @@ describe("Test createInterface method of readline", function(err){
            readline.Interface.prototype.on.restore();
            sinon.assert.calledWith(stub,"line");
 
-    });
+   });
    });
 
-    describe("Test on method of Interface for close event", function(err){
+   describe("Test on method of Interface for close event", function(err){
     it("should be called", function() {
            var stub = sinon.stub(readline.Interface.prototype,'on');
            convert(2016);
@@ -69,4 +69,4 @@ describe("Test createInterface method of readline", function(err){
            sinon.assert.calledWith(stub,"close");
     });
 
- 	});
+    });
