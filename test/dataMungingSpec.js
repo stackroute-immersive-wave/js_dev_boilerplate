203c4f2ed1144a753c63e496ceb1758d6dd06b10
pradhi <pradhi@gmail.com>
2017-05-09 14:54:16 +0530

    Modified Datamunging by pradheep

--- test/dataMungingSpec.js
+++ test/dataMungingSpec.js
@@ -4,13 +4,9 @@ expect = require("chai").expect,
 sinon = require('sinon'),
 readline = require("readline"),
 fs=require("fs"),
-<<<<<<< HEAD
 convert = require("../js/indiaCensusPradheep.js");
 //convert = require("../js/");
-=======
 
-convert = require("../js/lifeExpectancyKavya.js");
->>>>>>> a4acdeda2d77b624c9b878e4fb2cc3942e73ddc5
 
 describe("A series of test for Converting  CSV to JSON",
     function(err){
@@ -30,12 +26,8 @@ describe("A series of test for Converting  CSV to JSON",
         expect(convert.bind(undefined, {})).to.throw(Error, "Not a number");
         done();
     });
-<<<<<<< HEAD
-   it('should fail if year is NaN', function(done){
-=======
 
-  it('should fail if year is NaN', function(done){
->>>>>>> a4acdeda2d77b624c9b878e4fb2cc3942e73ddc5
+  it('should fail if year is NaN', function(done){}
         expect(convert.bind(undefined, NaN)).to.throw(Error, "Not a number");
         done();
     });
@@ -49,7 +41,6 @@ describe("A series of test for Converting  CSV to JSON",
         expect(convert.bind(undefined, Number(1960))).to.not.throw(Error, "Not a number");
         done();
     });
-<<<<<<< HEAD
 
    describe("Test createInterface method of readline", function(err){
         it("should be called only once", function() {
@@ -79,36 +70,3 @@ describe("A series of test for Converting  CSV to JSON",
    });
 
  });
-=======
-});
-
-
-describe("Test createInterface method of readline", function(err){
-        it("should be called only once", function() {
-            var spyCreateInterface = sinon.spy(readline, 'createInterface');
-            convert(2016);
-            readline.createInterface.restore();
-            sinon.assert.calledOnce(spyCreateInterface);
-    });
-  });
-    describe("Test on method of Interface for line event", function(err){
-    it("should be called", function() {
-           var stub = sinon.stub(readline.Interface.prototype, 'on');
-           convert(2016);
-           sinon.assert.called(stub);
-           readline.Interface.prototype.on.restore();
-           sinon.assert.calledWith(stub,"line");
-
-   });
-   });
-
-   describe("Test on method of Interface for close event", function(err){
-    it("should be called", function() {
-           var stub = sinon.stub(readline.Interface.prototype,'on');
-           convert(2016);
-           readline.Interface.prototype.on.restore();
-           sinon.assert.calledWith(stub,"close");
-    });
-
-    });
->>>>>>> a4acdeda2d77b624c9b878e4fb2cc3942e73ddc5
