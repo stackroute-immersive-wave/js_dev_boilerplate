7493b8167190b21c870d18fe8a3c7d39578fc109
santhoshkumar <Santhoshskbe@gmail.com>
2017-05-09 12:29:01 +0530

    changed datamunging file

--- test/dataMungingSpec.js
+++ test/dataMungingSpec.js
@@ -4,7 +4,7 @@ sinon = require('sinon'),
 readline = require("readline"),
 fs=require("fs"),
 
-convert = require("../js/g20Mitali");
+convert = require("../js/gDP.js");
 
 
 describe("A series of test for Converting  CSV to JSON",
@@ -36,3 +36,31 @@ describe("A series of test for Converting  CSV to JSON",
         done();
     });
  });
+describe("Test createInterface method of readline", function(err){
+        it("should be called only once", function() {
+           var spyCreateInterface = sinon.spy(readline, 'createInterface');
+           convert(2016);
+           readline.createInterface.restore();
+           sinon.assert.calledOnce(spyCreateInterface);
+   });
+ });
+   describe("Test on method of Interface for line event", function(err){
+   it("should be called", function() {
+          var stub = sinon.stub(readline.Interface.prototype, 'on');
+          convert(2016);
+          sinon.assert.called(stub);
+          readline.Interface.prototype.on.restore();
+          sinon.assert.calledWith(stub,"line");
+
+   });
+  });
+
+   describe("Test on method of Interface for close event", function(err){
+   it("should be called", function() {
+          var stub = sinon.stub(readline.Interface.prototype,'on');
+          convert(2016);
+          readline.Interface.prototype.on.restore();
+          sinon.assert.calledWith(stub,"close");
+   });
+
+    });
