5c7fe09fa3c416acf4143cc8f4d323a575b21dbb
Ximanta <ximanta.sarma@gmail.com>
2017-05-16 06:42:14 +0530

    Removed black box testing

--- test/dataMungingSpec.js
+++ test/dataMungingSpec.js
@@ -1,36 +1,23 @@
-
-
-
+/*eslint-disable*/
 const should = require("chai").should(),
 expect = require("chai").expect,
 sinon = require('sinon'),
 readline = require("readline"),
 fs=require("fs"),
-
-
-convert = require('../js/cicagoCrimeHema');
+convert = require('../js/indiaCensusStructure');
 
 
 describe("A series of test for Converting  CSV to JSON",
   function(err){
-
-
-
     it("should return sucess message", function(done){
      var result = convert(2001);
      result.should.be.equal('JSON written successfully');
      done();
    });
-
-
-   /* it('should fail if year is notprovided', function(done){
-=======
     it('should fail if year is notprovided', function(done){
->>>>>>> b32ee26406205bc5582de541110666b1d2948076
       expect(convert).to.throw(Error, "Not a number");
       done();
     });
-
     it('should fail if year is not a number', function(done){
       expect(convert.bind(undefined, {})).to.throw(Error, "Not a number");
       done();
@@ -39,21 +26,16 @@ describe("A series of test for Converting  CSV to JSON",
     it('should fail if year is NaN', function(done){
       expect(convert.bind(undefined, NaN)).to.throw(Error, "Not a number");
       done();
-<<<<<<< HEAD
-    });*/
-
+    });
 
     it('should not fail if the year is a literal number', function(done){
       expect(convert.bind(undefined, '1960')).to.not.throw(Error, "Not a number");
       done();
     });
-
     it('should not fail if the year is a Number object', function(done){
       expect(convert.bind(undefined, Number(1960))).to.not.throw(Error, "Not a number");
       done();
     });
-
-
 });
 
 describe("Test createInterface method of readline", function(err){
@@ -84,9 +66,3 @@ describe("Test createInterface method of readline", function(err){
  });
 
 });
-
-
-
-
-
-
